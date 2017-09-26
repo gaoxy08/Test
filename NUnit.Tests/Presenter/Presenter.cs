e5a0776a616b386beccba1eef716b953b0947c2f
18626299163 <18626299163@sina.cn>
2017-09-26 18:19:29 +0800

    重构Presenter

---
+++ NUnit.Tests/Presenter/Presenter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presenter
+{
+    public class Presenter
+    {
+        private readonly IView _view;
+        public Presenter(IView view)
+        {
+            _view = view;
+            this._view.Loaded += _view_Loaded;
+        }
+
+        private void _view_Loaded()
+        {
+            _view.Render("Hello World");
+        }
+    }
+
+    public interface IView
+    {
+        event Action Loaded;
+        void Render(string text);
+    }
+}
