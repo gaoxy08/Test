5dbafb8bcebdda4039dc8de50d0696d3b9f35269
18626299163 <18626299163@sina.cn>
2017-09-26 17:45:36 +0800

    新添加了一个GetExposureTime方法

---
+++ NUnit.Tests/NUnit.Tests/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            // 新的测试范例是是是
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
