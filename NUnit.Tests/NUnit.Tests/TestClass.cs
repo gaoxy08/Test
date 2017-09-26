d57b4c232bdf2ac2eefdeaa0a18a44656b16e7c8
18626299163 <18626299163@sina.cn>
2017-09-26 18:51:29 +0800

    1. 创建事件测试方法 2. 添加NSunstitute隔离框架

--- NUnit.Tests/NUnit.Tests/TestClass.cs
+++ NUnit.Tests/NUnit.Tests/TestClass.cs
@@ -1,26 +1,27 @@
-﻿using NUnit.Framework;
+﻿using NSubstitute;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Presenter;
 
 namespace NUnit.Tests
 {
     [TestFixture]
-    public class TestClass
+    public class EventRelatedTests
     {
         [Test]
-        public void TestMethod()
+        public void ctor_WhenViewIsLoaded_CallsViewRender()
         {
-            // TODO: Add your test code here
-            // 新的测试范例是是是
-            Assert.Pass("Your first passing test");
+            var mockView = Substitute.For<IView>();
+
+            Presenter.Presenter p = new Presenter.Presenter(mockView);
+            mockView.Loaded += Raise.Event<Action>();
+
+            mockView.Received().Render(Arg.Is<string>(s => s.Contains("Hello World")));
         }
     }
 
-    public interface ILogger
-    {
-        void WriteMessage(string message);
-    }
 }
